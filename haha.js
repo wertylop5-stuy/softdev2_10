8705e760596c1bf928eb67ff37cd82cbb29a6cb1
Stanley Lin <slin55055@gmail.com>
2018-03-07 18:24:07 -0500

    finished

--- haha.js
+++ haha.js
@@ -1,74 +1,94 @@
 "use strict";
 
 const SVG_NS = "http://www.w3.org/2000/svg";
-const CHANGED_CLASS = "changed";
-const CIRCLE_CLASS = "circ";
 
-let svg;
+let circles;
+
+class Circle {
+	constructor(cx, cy, svg) {
+		let that = this
+		this.elem = Circle.createElem(cx, cy);
+		this.elem.addEventListener("click", e => {
+			that.toggle(e);
+		});
+		this.svg = svg;
+		this.toggled = false;
+
+		this.svg.appendChild(this.elem);
+	}
+
+	static setAttr(obj, attr) {
+		for (let a in attr) {
+			obj.setAttribute(a, attr[a]);
+		}
+	}
+
+	static createElem(cx, cy) {
+		let temp = document.createElementNS(SVG_NS, "circle");
+		Circle.setAttr(temp, {
+			cx,
+			cy,
+			r: 20,
+			stroke: "black",
+			fill: "BlanchedAlmond"
+		});
+		
+		return temp;
+	}
+
+	toggle(e) {
+		if (this.toggled) {
+			let maxX = this.svg.getAttribute("width");
+			let maxY = this.svg.getAttribute("height");
+			let that = this;
+			
+			this.svg.removeChild(this.elem);
+			this.elem = Circle.createElem(randMax(maxX), randMax(maxY));
+			this.elem.addEventListener("click", e => {
+				that.toggle(e);
+			});
+
+			this.svg.appendChild(this.elem);
+			this.toggled = false;
+		}
+		else {
+			Circle.setAttr(this.elem, {
+				fill: "green"
+			});
+			
+			this.toggled = true;
+		}
+		
+		e.stopPropagation();
+	}
+	
+	removeElem() {
+		this.svg.removeChild(this.elem);
+	}
+}
 
 function randMax(max) {
 	return Math.random()*max;
 }
 
-function createCircle(cx, cy) {
-	let temp = document.createElementNS(SVG_NS, "circle");
-	setAttr(temp, {
-		cx,
-		cy,
-		r: 20,
-		stroke: "black",
-		fill: "BlanchedAlmond"
-	});
-	temp.classList.add(CIRCLE_CLASS);
-	temp.addEventListener("click", doThing);
-	
-	return temp;
-}
-
-function doThing(e) {
-	if (e.target.classList.contains(CHANGED_CLASS)) {
-		let maxX = svg.getAttribute("width");
-		let maxY = svg.getAttribute("height");
-		
-		svg.removeChild(e.target);
-		let temp = createCircle(randMax(maxX), randMax(maxY));
-
-		svg.appendChild(temp);
-	}
-	else {
-		setAttr(e.target, {
-			fill: "green"
-		});
-		e.target.classList.add(CHANGED_CLASS);
-	}
-	
-	e.stopPropagation();
-}
-
-function setAttr(obj, attr) {
-	for (let a in attr) {
-		obj.setAttribute(a, attr[a]);
-	}
-}
-
 (() => {
-	svg = document.getElementById("boi");
+	let svg = document.getElementById("boi");
 	let clear = document.getElementById("clear");
+	circles = [];
 	
 	svg.addEventListener("click", e => {
 		let cx = e.offsetX;
 		let cy = e.offsetY;
-		let temp = createCircle(cx, cy);
 		
-		svg.appendChild(temp);
+		circles.splice(0, 0, new Circle(cx, cy, svg));
 	});
 
 	clear.addEventListener("click", e => {
-		let l = document.getElementsByClassName(CIRCLE_CLASS);
-		for (let x = l.length-1; x >= 0; x--) {
-			svg.removeChild(l[x]);
-			console.log(l);
+		for (let x = circles.length-1; x >= 0; x--) {
+			circles[x].removeElem();
 		}
+		circles.splice(0, circles.length);
 	});
+
 })();
 
